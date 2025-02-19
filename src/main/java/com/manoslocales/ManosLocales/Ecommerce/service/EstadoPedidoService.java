8a610ac0e841561ed2ebe41230eec7df58e00c54
MSaldarriagaGomez <maria.antonieta613@gmail.com>
2025-02-19 17:08:02 -0500

    estado updated

--- src/main/java/com/manoslocales/ManosLocales/Ecommerce/service/EstadoPedidoService.java
+++ src/main/java/com/manoslocales/ManosLocales/Ecommerce/service/EstadoPedidoService.java
@@ -1,10 +1,14 @@
 package com.manoslocales.ManosLocales.Ecommerce.service;
 
 
+import com.manoslocales.ManosLocales.Ecommerce.Exceptions.GeneralException;
+import com.manoslocales.ManosLocales.Ecommerce.model.EstadoPedido;
 import com.manoslocales.ManosLocales.Ecommerce.repository.IestadoPedidoRepository;
 import com.manoslocales.ManosLocales.Ecommerce.service.interfaces.IestadoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EstadoPedidoService implements IestadoPedidoService {
@@ -14,5 +18,36 @@ public class EstadoPedidoService implements IestadoPedidoService {
     @Autowired
     public EstadoPedidoService(IestadoPedidoRepository iestadoPedidoRepository) {
         this.iestadoPedidoRepository = iestadoPedidoRepository;
+    }
+
+    @Override
+    public EstadoPedido createEstadoPedido(EstadoPedido estadoPedido) {
+      return this.iestadoPedidoRepository.save(estadoPedido);
+    }
+
+    @Override
+    public EstadoPedido findbyId(Long id) {
+        Optional<EstadoPedido> optionalEstadoPedido= this.iestadoPedidoRepository.findById(id);
+        if (optionalEstadoPedido.isPresent())
+        {return optionalEstadoPedido.get();
+        }
+        throw new GeneralException("El id del estatado del producto no existe");
+    }
+
+    @Override
+    public EstadoPedido updateEstadoPedido(Long id,EstadoPedido estadoPedido) {
+        Optional<EstadoPedido> estadoOptional = this.iestadoPedidoRepository.findById(id);
+
+        if (estadoOptional.isPresent()) {
+            EstadoPedido estadoToUpdate = estadoOptional.get();
+
+            if (estadoPedido.getDescripcion() != null && !estadoPedido.getDescripcion().trim().isEmpty()) {
+                estadoToUpdate.setDescripcion(estadoPedido.getDescripcion());
+            }
+
+            return iestadoPedidoRepository.save(estadoToUpdate);
+        } else {
+            throw new GeneralException("El estado de pedido no existe con id " + id);
+        }
     }
 }
--- src/main/java/com/manoslocales/ManosLocales/Ecommerce/service/interfaces/IestadoPedidoService.java
+++ src/main/java/com/manoslocales/ManosLocales/Ecommerce/service/interfaces/IestadoPedidoService.java
@@ -1,4 +1,11 @@
 package com.manoslocales.ManosLocales.Ecommerce.service.interfaces;
 
+import com.manoslocales.ManosLocales.Ecommerce.model.EstadoPedido;
+
 public interface IestadoPedidoService {
+
+    EstadoPedido createEstadoPedido(EstadoPedido estadoPedido);
+    EstadoPedido findbyId(Long id);
+    EstadoPedido updateEstadoPedido(Long id,EstadoPedido estadoPedido);
+
 }
